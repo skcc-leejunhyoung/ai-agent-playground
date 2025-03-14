ee76895b633757f331f4eba322d522b0ed2a1231
Junhyoung <junhyoung@skcc.com>
2025-03-14 16:55:46 +0900

    UPDATE: result table

--- pages/main_playground.py
+++ pages/main_playground.py
@@ -123,7 +123,7 @@ if st.session_state["results"]:
             height=500,
         )
         option_map = {
-            0: ":material/add:",
+            0: ":material/docs:",
             1: ":material/zoom_in:",
             2: ":material/zoom_out:",
             3: ":material/zoom_out_map:",
@@ -135,3 +135,19 @@ if st.session_state["results"]:
             selection_mode="single",
             label_visibility="collapsed",
         )
+        if selection == 0:
+            project_dict = dict(st.session_state["project"])
+            current_session_id = project_dict.get("current_session_id", 0)
+
+            all_results = get_results_by_project(project_id)
+
+            current_session_results = [
+                r for r in all_results if r["session_id"] == current_session_id
+            ]
+
+            if not current_session_results:
+                st.warning(f"Session #{current_session_id}의 결과가 없습니다.")
+            else:
+                st.dataframe(
+                    reversed(current_session_results), use_container_width=True
+                )
